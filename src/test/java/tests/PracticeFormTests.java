f95fa07b8b39e2a228a324e97ece1b100965d855
julia karnausova <julianaas@yandex.ru>
2025-08-14 21:33:34 +0300

    feat: add choose radio btn

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,4 +1,52 @@
 package tests;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+//        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        String route = "/automation-practice-form";
+        String firstName = "Julia";
+        String lastName = "K";
+        String email = "test_email@ya.ru";
+
+        open(route);
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText("Female")).click();
+
+        // mobile
+        // date of birth
+        // subjects
+        // hobbies
+        // picture
+        $("#currentAddress").setValue("Some street 1");
+        // state and city + city
+        $("#submit").click();
+
+        // modal window selector
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
 }
