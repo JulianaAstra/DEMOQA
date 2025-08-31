0e8342118a41cbb10f0a76cc618c0943a942faf7
Julia <iuliia.karnausova@enkod.io>
2025-08-31 12:39:16 +0300

    feat: add textbox tests + pageobject

---
+++ src/test/java/Pages/TextBoxPage.java
@@ -0,0 +1,65 @@
+package Pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxPage {
+    String textBoxPageRoute = "/text-box";
+
+    SelenideElement
+        firstNameInput = $("#userName"),
+        emailInput =  $("#userEmail"),
+        currentAddressInput = $("#currentAddress"),
+        permanentAddressInput = $("#permanentAddress"),
+        submitButton = $("#submit"),
+        firsNameOutput =  $("#output #name"),
+        emailOutput = $("#output #email"),
+        currentAddressOutput = $("#output #currentAddress"),
+        permanentAddressOutput = $("#output #permanentAddress");
+
+    public TextBoxPage openTextBoxPage() {
+        open(textBoxPageRoute);
+        return this;
+    }
+
+    public TextBoxPage setUserFirstName(String userFirstName) {
+        firstNameInput.setValue(userFirstName);
+        return this;
+    }
+
+    public TextBoxPage setUserEmail(String userEmail) {
+        emailInput.setValue(userEmail);
+        return this;
+    }
+
+    public TextBoxPage setUserCurrentAddress(String userCurrentAddress) {
+        currentAddressInput.setValue(userCurrentAddress);
+        return this;
+    }
+
+    public TextBoxPage setUserPermanentAddress(String userPermanentAddress) {
+        permanentAddressInput.setValue(userPermanentAddress);
+        return this;
+    }
+
+    public TextBoxPage sendForm() {
+        submitButton.click();
+        return this;
+    }
+
+    public TextBoxPage checkOutput(
+            String userFirstName,
+            String userEmail,
+            String userCurrentAddress,
+            String userPermanentAddress) {
+        firsNameOutput.shouldHave(text(userFirstName));
+        emailOutput.shouldHave(text(userEmail));
+        currentAddressOutput.shouldHave(text(userCurrentAddress));
+        permanentAddressOutput.shouldHave(text(userPermanentAddress));
+
+        return this;
+    }
+}
--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,36 +1,33 @@
 package tests;
-
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import Pages.TextBoxPage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+public class TextBoxTests extends TestBase {
+    TextBoxPage textBoxPage;
+    String firstName;
+    String email;
+    String currentAddress;
+    String permanentAddress;
 
-public class TextBoxTests {
-
-    @BeforeAll
-    static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+    @BeforeEach
+    public void setData() {
+        textBoxPage = new TextBoxPage();
+        firstName = "Alex";
+        email = "alex@egorov.com";
+        currentAddress = "Some street 1";
+        permanentAddress = "Another street 1";
     }
 
     @Test
     void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
-        $("#submit").click();
-
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        textBoxPage
+            .openTextBoxPage()
+            .setUserFirstName(firstName)
+            .setUserEmail(email)
+            .setUserCurrentAddress(currentAddress)
+            .setUserPermanentAddress(permanentAddress)
+            .sendForm()
+            .checkOutput(firstName, email, currentAddress, permanentAddress);
     }
 }
